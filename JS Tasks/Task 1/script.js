741fa4ee878737174ad346e8135f44b60bbb6b48
EditaKlu <edita.klusiene@gmail.com>
2022-12-07 19:26:21 +0200

    1 task done

--- JS Tasks/Task 1/script.js
+++ JS Tasks/Task 1/script.js
@@ -8,3 +8,21 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+
+const weightInput = document.querySelector('#search');
+const submitBtn = document.querySelector('#submit-btn');
+const convertedWeight = document.querySelector('#output');
+
+submitBtn.addEventListener('click', myFunction);
+
+function myFunction(event) {
+    event.preventDefault();
+    convertedWeight.innerHTML = '';
+    const lbWeight = weightInput.value * 2.2046;
+    const gWeight = weightInput.value / 0.0010000;
+    const ozWeight = weightInput.value * 35.274;
+    convertedWeight.innerHTML += `<div id='outputStyles'>
+    <div> Pounds: ${lbWeight} lb. </div>
+    <div> Grams: ${gWeight} g. </div>
+    <div> Ounces: ${ozWeight} oz. </div></div>`
+}

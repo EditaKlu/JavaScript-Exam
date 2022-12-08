c865871d40cfe4e7909eab3e6985069486265671
EditaKlu <edita.klusiene@gmail.com>
2022-12-08 18:26:25 +0200

    8 task done

--- JS Tasks/Task 8/script.js
+++ JS Tasks/Task 8/script.js
@@ -5,3 +5,30 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(a, b) {
+    this.a = a;
+    this.b = b;
+    this.sum = function () {
+        return this.a + this.b;
+    }
+    this.substraction = function () {
+        return this.a - this.b;
+    }
+    this.multiplication = function () {
+        return this.a * this.b;
+    }
+    this.division = function () {
+        return this.a / this.b;
+    }
+}
+const sumcount = new Calculator(1, 4);
+const subscount = new Calculator(5, 4);
+const multicount = new Calculator(2, 4);
+const divcount = new Calculator(8, 2);
+
+console.log(sumcount.sum());
+console.log(subscount.substraction());
+console.log(multicount.multiplication());
+console.log(divcount.division());
+

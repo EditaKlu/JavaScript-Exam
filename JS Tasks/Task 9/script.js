2806972919048ba0aa115c0e950a104971b98fcb
EditaKlu <edita.klusiene@gmail.com>
2022-12-08 19:09:04 +0200

    9 task done

--- JS Tasks/Task 9/script.js
+++ JS Tasks/Task 9/script.js
@@ -9,3 +9,20 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+    wasExpensive() {
+        return console.log(this.budget > 100000000);
+    }
+}
+
+const firstMovie = new Movie('Avengers: Age of Ultron', 'Joss Whedon', 365000000);
+const secondMovie = new Movie('Die Hard 2', 'Renny Harlin', 70000000);
+const thirdMovie = new Movie('Maleficent', 'Robert Stromberg', 226000000);
+firstMovie.wasExpensive();
+secondMovie.wasExpensive();
+thirdMovie.wasExpensive();
